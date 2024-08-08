bdab43aa8a0dd596c193920dd82ff8ef6809ceb0
Dwnial <danialhaghani007@gmail.com>
2024-08-08 23:08:31 +0330

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,16 @@
+print ("menu:")
+print("jam=1")
+print("menha=2")
+print("zarb=3")
+print("taghsim=4")
+z=int(input("enter your num:"))
+x=float(input("enter num 1:"))
+y=float(input("enter num 2:"))
+if z==1 :
+ print(x+y)
+elif z==2 :
+ print(x-z)
+elif z==3 :
+ print (x*y)
+elif z==4 :
+ print(x/y)
